d266ae8c360c710660ee132dad667466baaf8757
johnmanjiro13 <juntama193@gmail.com>
2021-02-03 18:58:48 +0900

    switch all commands

--- command/service.go
+++ command/service.go
@@ -32,34 +32,29 @@ func (s *service) GetCommand(content string) DokkoiCmd {
 	// replace full-width whitespace to half size whitespace
 	replacedContent := strings.Replace(content, "　", " ", -1)
 	cmd := strings.Split(replacedContent, " ")
-	if (len(cmd) <= 1 || cmd[0] != "dokkoi") && !(strings.HasSuffix(content, IncrOperator) || strings.HasSuffix(content, DecrOperator)) {
-		return nil
-	}
-	switch cmd[1] {
-	case "help":
+	switch {
+	case len(cmd) >= 2 && cmd[1] == "help":
 		return &helpCmd{target: strings.Join(cmd[2:], " ")}
-	case "echo":
+	case len(cmd) >= 3 && cmd[1] == "echo":
 		return &echoCmd{message: strings.Join(cmd[2:], " ")}
-	case "image":
+	case len(cmd) >= 3 && cmd[1] == "image":
 		return &imageCmd{
 			customSearchRepo: s.customSearchRepo,
 			query:            strings.Join(cmd[2:], " "),
 		}
-	}
-
-	// check score command
-	if strings.HasSuffix(content, IncrOperator) {
+	case strings.HasSuffix(content, IncrOperator):
 		return &scoreCmd{
 			scoreRepo: s.scoreRepo,
 			user:      content[:len(content)-2],
 			operator:  IncrOperator,
 		}
-	} else if strings.HasSuffix(content, DecrOperator) {
+	case strings.HasSuffix(content, DecrOperator):
 		return &scoreCmd{
 			scoreRepo: s.scoreRepo,
 			user:      content[:len(content)-2],
 			operator:  DecrOperator,
 		}
+	default:
+		return nil
 	}
-	return nil
 }
