53f120e5fd08e1060fd3163399b7d4d27aa62b88
johnmanjiro13 <juntama193@gmail.com>
2021-02-06 14:45:49 +0900

    pass image search

--- command/image_test.go
+++ command/image_test.go
@@ -14,20 +14,12 @@ func TestImageCmd_searchImage(t *testing.T) {
 	tests := map[string]struct {
 		query    string
 		image    *customsearch.Result
-		err      error
 		expected string
 	}{
 		"normal success": {
 			query:    "dragapult",
 			image:    &customsearch.Result{Link: "https://example.com/dragapult.jpeg"},
-			err:      nil,
 			expected: "https://example.com/dragapult.jpeg",
-		},
-		"not found": {
-			query:    "not found",
-			image:    nil,
-			err:      ErrImageNotFound,
-			expected: "image was not found",
 		},
 	}
 
@@ -37,7 +29,7 @@ func TestImageCmd_searchImage(t *testing.T) {
 
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
-			mockCustomSearchRepo.EXPECT().SearchImage(tt.query).Return(tt.image, tt.err)
+			mockCustomSearchRepo.EXPECT().SearchImage(tt.query).Return(tt.image, nil)
 			cmd := &imageCmd{
 				customSearchRepo: mockCustomSearchRepo,
 				query:            tt.query,
