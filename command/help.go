5ff75e3faa96e5ce3d9fe27b58d03f276061a53e
johnmanjiro13 <juntama193@gmail.com>
2021-08-15 19:00:20 +0900

    add lgtm to help list

--- command/help.go
+++ command/help.go
@@ -15,6 +15,7 @@ var descriptions = map[string]string{
 	"help":   "dokkoi help - Displays all of the help commands that this bot knows about.\ndokkoi help <query> - Displays all help commands that match <query>.",
 	"echo":   "dokkoi echo <text> - Reply back with <text>",
 	"image":  "dokkoi image <query> - Queries Google Images for <query> and returns a top result.",
+	"lgtm":   "dokkoi lgtm <query> - Queries Google Images for <query> and returns a top result with LGTM text.",
 	"score":  "dokkoi score <name> - Display the score for a name.",
 	"select": "dokkoi select <element1>,<element2>,... - Choose one of the elements in your list randomly.",
 	"++":     "<name>++ - Increment score for a name",
